7f485ce31dfde287ac2bfa67ed98726070a0fe81
Daniel <danielmrezende@yahoo.com.br>
2023-06-08 10:28:19 -0300

    Estruturas iniciais

---
+++ Sources/main.cpp
@@ -0,0 +1,18 @@
+#include <chrono>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <math.h>
+#include <sstream>
+#include <stdlib.h>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+int main () {
+    return 0;
+}
