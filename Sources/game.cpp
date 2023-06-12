be04e245e1f5036fbfe89ddab969338874160d89
Daniel <danielmrezende@yahoo.com.br>
2023-06-12 16:53:13 -0300

    Desenvolvendo Bactracking

--- Sources/game.cpp
+++ Sources/game.cpp
@@ -1,5 +1,10 @@
+#include <algorithm>
 #include <iostream>
+#include <sstream>
+#include <stdlib.h>
 #include <string>
+#include <tuple>
+#include <list>
 #include "../Headers/game.h"
 
 using namespace std;
@@ -27,4 +32,78 @@ void Game::print_board(){
         cout << board[i] << " ";
     }
     cout << "]" << endl;
+}
+
+bool Game::verify_win(){
+    return count_white == 0;
+}
+
+Game *Game::build_child(){
+    Game *aux;
+
+    aux->n_pieces = this->n_pieces;
+    aux->size_board = this->size_board;
+    aux->count_white = this->count_white;
+    aux->count_move = this->count_move;
+    aux->board = new char[this->n_pieces];
+
+    for(int i=0; i<this->size_board; i++){
+        aux->board[i] = this->board[i];
+    }
+
+    return aux;
+}
+
+/**
+ * @brief Verifica os movimentos possíveis a partir de uma conifugração de tabuleiro;
+ * 
+ * @param three            Recebe a árvore da solução;
+ * @return list<tuple<int, int>>  Retorna a lista de movimentos possíveis;
+ */
+list<tuple<int, int>> Game::possible_moves(list<Game *> *three){
+    list<tuple<int, int>> moves;
+
+    for(int i=0; i<this->size_board; i++){
+        if(this->board[i] != '-' && this->board[i+1] == '-'){
+            Game *aux = this->build_child();
+            aux->board[i+1] = aux->board[i];
+            aux->board[i] = '-';
+
+            if(!(std::find(three->begin(), three->end(), aux->board) != three->end())){
+                moves.push_back(tuple<int, int>(i, i+1));
+            }
+        }
+
+        if(this->board[i] != '-' && this->board[i+2] == '-'){
+            Game *aux = this->build_child();
+            aux->board[i+2] = aux->board[i];
+            aux->board[i] = '-';
+
+            if(!(std::find(three->begin(), three->end(), aux->board) != three->end())){
+                moves.push_back(tuple<int, int>(i, i+2));
+            }
+        }
+
+        if(this->board[i] != '-' && this->board[i-1] == '-'){
+            Game *aux = this->build_child();
+            aux->board[i-1] = aux->board[i];
+            aux->board[i] = '-';
+
+            if(!(std::find(three->begin(), three->end(), aux->board) != three->end())){
+                moves.push_back(tuple<int, int>(i, i-1));
+            }
+        }
+
+        if(this->board[i] != '-' && this->board[i-2] == '-'){
+            Game *aux = this->build_child();
+            aux->board[i-2] = aux->board[i];
+            aux->board[i] = '-';
+
+            if(!(std::find(three->begin(), three->end(), aux->board) != three->end())){
+                moves.push_back(tuple<int, int>(i, i-2));
+            }
+        }
+    }
+
+    return moves;
 }
--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <cstdlib>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
@@ -38,7 +39,71 @@ int main () {
         cout << endl;
         cout << "------------------------------------------" << endl;
 
-        
+        int option = 9000;
+        std::chrono::time_point<std::chrono::system_clock> init, final;
+
+        while (option != 0){
+            cout << "Funcionalidades: " << endl;
+            cout << "   - 1 - Backtracking;" << endl;
+            cout << "   - 2 - Busca em Profundidade" << endl;
+            cout << "   - 3 - Busca em Largura" << endl;
+            cout << "   - 4 - Busca Ordenada" << endl;
+            cout << "   - 5 - Busca Gulosa" << endl;
+            cout << "   - 6 - Busca A*" << endl;
+            cout << "   - 7 - Busca IDA*" << endl;
+            cout << "   - 0 - Sair do Programa" << endl;
+            cout << "-------------------------------" << endl;
+            cout << "Escolha uma opcao: ";
+            cin >> option;
+            cout << endl;
+            cout << "------------------------------------------" << endl;
+
+            switch (option){
+                case 1:
+                    init = chrono::high_resolution_clock::now();
+                    // Chamar algoritmo aqui;
+                    final = chrono::high_resolution_clock::now();
+                    break;
+                case 2:
+                    init = chrono::high_resolution_clock::now();
+                    // Chamar algoritmo aqui;
+                    final = chrono::high_resolution_clock::now();
+                    break;
+                case 3:
+                    init = chrono::high_resolution_clock::now();
+                    // Chamar algoritmo aqui;
+                    final = chrono::high_resolution_clock::now();
+                    break;
+                case 4:
+                    init = chrono::high_resolution_clock::now();
+                    // Chamar algoritmo aqui;
+                    final = chrono::high_resolution_clock::now();
+                    break;
+                case 5:
+                    init = chrono::high_resolution_clock::now();
+                    // Chamar algoritmo aqui;
+                    final = chrono::high_resolution_clock::now();
+                    break;
+                case 6:
+                    init = chrono::high_resolution_clock::now();
+                    // Chamar algoritmo aqui;
+                    final = chrono::high_resolution_clock::now();
+                    break;
+                case 7:
+                    init = chrono::high_resolution_clock::now();
+                    // Chamar algoritmo aqui;
+                    final = chrono::high_resolution_clock::now();
+                    break;
+                case 0:
+                    cout << "                            ...Saindo...                            " << endl;
+                    cout << "================================ BY ================================" << endl;
+                    continue;
+                default:
+                    cout << "Opcao invalida." << endl;
+                    cout << "-------------------------------" << endl;
+                    continue;
+            }
+        }
 
         return 0;
     }
